bb577f2124610bc9d4e5bf125c202a8aab9d8ca4
J M <J M>
2016-11-11 14:41:11 -0800

    texts added

--- src/Visuals/TwitterPanel.java
+++ src/Visuals/TwitterPanel.java
@@ -13,13 +13,15 @@ import javax.swing.Timer;
 
 import twitter4j.Status;
 
-public class TwitterPanel extends JPanel implements ActionListener {
+
+public class TwitterPanel extends JPanel  {
+	private ArrayList<Status> status;
+	
 	public TwitterPanel(ArrayList<Status> status) {
 		this.setVisible(true);
 		this.setSize(Display.F_WIDTH / 2, Display.F_HEIGHT);
-		this.setBackground(Color.YELLOW);
-		 Timer timer = new Timer(20, this);
-		 timer.start();
+		this.setBackground(Color.BLUE);
+		this.status = status;
 	}
 
 	public static void main(String[] args) {
@@ -33,30 +35,18 @@ public class TwitterPanel extends JPanel implements ActionListener {
 	protected void paintComponent(Graphics g) {
 		// Call the "paint" method in the parent class
 		super.paintComponent(g);
-
-		System.out.println("drawing");
-		// Draw a blue line
-		g.setColor(Color.BLUE);
-		g.drawLine(45, 25, 445, 25);
-
-		// Draw a red rectangle
 		g.setColor(Color.RED);
-		g.drawRect(45, 50, 400, 285);
-
-		// Draw some green text
-		g.setColor(Color.GREEN);
-		g.setFont(new Font("Arial", Font.BOLD, 30));
-		g.drawString("Hello, World!", 155, 120);
-
-		// Draw a filled-in magenta circle
-		g.setColor(Color.MAGENTA);
-		g.fillOval(200, 180, 100, 100);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		System.out.println("test");
-		
-		
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("Arial", Font.BOLD, 10));
+		int y = 155;
+		for (Status tweet: status)
+		{
+			g.drawString(tweet.getText(), 0, y);
+			y+=30;
+			if (y > getHeight())
+			{
+				break;
+			}
+		}
 	}
 }
