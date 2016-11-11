83a2beedcc9af2bfdb3312f7b67fba3c4400266c
Austin Patel <austinapatel@gmail.com>
2016-11-10 20:39:25 -0800

    Added the TwitterAPI class

---
+++ src/twitter_api/TwitterAPI.java
@@ -0,0 +1,45 @@
+
+package twitter_api;
+
+import java.util.ArrayList;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+/**Connects to the Twitter API through the Twitter4J library.
+ * Provides static methods to perform various functions on Twitter.*/
+public class TwitterAPI {
+
+	/**Tests the TwitterAPI class.*/
+	public static void main(String[] args) {
+		ArrayList<Status> searchResults = searchTwitter("california election", 10);
+		
+		for (Status searchResult : searchResults)
+			System.out.println('@' + searchResult.getUser().getScreenName() + ": " + searchResult.getText());
+	}
+
+	/**Searches Twitter given a query and number of results.*/
+	public static ArrayList<Status> searchTwitter(String search, int size) {
+		Twitter twitter = TwitterFactory.getSingleton();
+		
+		Query query = new Query(search);
+		query.setCount(size);
+		
+		ArrayList<Status> results = new ArrayList<Status>();
+
+		try {
+			QueryResult result = twitter.search(query);
+
+			for (Status status : result.getTweets())
+				 results.add(status);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+		
+		return results;
+	}
+}
